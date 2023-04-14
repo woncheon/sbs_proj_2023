6860d08d559c15fd5ae588b0c49ff9cc9835f2c5
kimhyeji <gpwl5589@gmail.com>
2023-04-14 18:45:40 +0900

    SQL을 xml 파일로 옮김

--- src/main/java/com/khj/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/khj/exam/demo/repository/ArticleRepository.java
@@ -2,32 +2,22 @@ package com.khj.exam.demo.repository;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 import com.khj.exam.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
-	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(@Param("id") int id);
 	
-	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
 	
-	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
 	public void writeArticle(@Param("title") String title, @Param("body") String body);
 	
-	@Delete("DELETE FROM article WHERE id = #{id}")
 	public void deleteArticle(@Param("id") int id);
 
-	@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
 	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
 
-	@Select("SELECT LAST_INSERT_ID()")
 	public int getLastInsertId();	
 }
