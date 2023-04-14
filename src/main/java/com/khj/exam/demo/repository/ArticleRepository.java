49e1c5e03e5ab002ff989d84fd70f1a5cd8b6e7b
kimhyeji <gpwl5589@gmail.com>
2023-04-14 17:44:16 +0900

    DELETE, UPDATE, SELECT 관련 쿼리를 메서드와 연결

--- src/main/java/com/khj/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/khj/exam/demo/repository/ArticleRepository.java
@@ -2,19 +2,29 @@ package com.khj.exam.demo.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.khj.exam.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
-	public Article getArticle(int id);
 	
+	@Select("SELECT * FROM article WHERE id = #{id}")
+	public Article getArticle(@Param("id") int id);
+	
+	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
+	
 	
 	public Article writeArticle(String title, String body);
 	
-	public void deleteArticle(int id);
+	@Delete("DELETE FROM article WHERE id = #{id}")
+	public void deleteArticle(@Param("id") int id);
 
-	public void modifyArticle(int id, String title, String body);	
+	@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW WHERE id = #{id}")
+	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);	
 }
