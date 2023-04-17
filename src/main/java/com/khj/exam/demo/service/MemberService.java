c0ed0ec022770056e7bcc76898cffa54bb628aad
kimhyeji <gpwl5589@gmail.com>
2023-04-17 15:14:32 +0900

    회원가입을 처리하기위해 UsrMemberController, MemberService 추가

---
+++ src/main/java/com/khj/exam/demo/service/MemberService.java
@@ -0,0 +1,12 @@
+package com.khj.exam.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+//	private MemberRepository memberRepository;
+	
+	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		
+	}
+}
