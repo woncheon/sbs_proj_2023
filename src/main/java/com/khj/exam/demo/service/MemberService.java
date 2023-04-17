561a8df0bce4d69fd9844ae3e7bb0502eec2dd90
kimhyeji <gpwl5589@gmail.com>
2023-04-17 16:07:57 +0900

    MemberRepository 추가 후, 회원가입 처리

--- src/main/java/com/khj/exam/demo/service/MemberService.java
+++ src/main/java/com/khj/exam/demo/service/MemberService.java
@@ -2,11 +2,17 @@ package com.khj.exam.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.khj.exam.demo.repository.MemberRepository;
+
 @Service
 public class MemberService {
-//	private MemberRepository memberRepository;
+	private MemberRepository memberRepository;
+	
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
 	
 	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-		
+		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 	}
 }
