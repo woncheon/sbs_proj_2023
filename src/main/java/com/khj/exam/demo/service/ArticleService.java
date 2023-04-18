baa4eb5f243f81ef9ff2a600fed5e73ff4c58cc8
woncheon <fhakdtm1991@naver,com>
2023-04-18 15:14:07 +0900

    do add resultData 적용

--- src/main/java/com/khj/exam/demo/service/ArticleService.java
+++ src/main/java/com/khj/exam/demo/service/ArticleService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.khj.exam.demo.repository.ArticleRepository;
+import com.khj.exam.demo.util.Ut;
 import com.khj.exam.demo.vo.Article;
+import com.khj.exam.demo.vo.ResultData;
 
 @Service
 public class ArticleService {
@@ -23,9 +25,10 @@ public class ArticleService {
 		return articleRepository.getArticles();
 	}
 	
-	public int writeArticle(String title, String body) {
+	public ResultData writeArticle(String title, String body) {
 		articleRepository.writeArticle(title, body);
-		return articleRepository.getLastInsertId();
+		int id= articleRepository.getLastInsertId();
+		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성 되었습니다", id), id);
 	}
 	
 	public void deleteArticle(int id) {
