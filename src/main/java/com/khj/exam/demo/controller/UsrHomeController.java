54f08e7c800dd7a76c8a89f333c4922dcf666f24
woncheon <fhakdtm1991@naver,com>
2023-04-19 15:14:17 +0900

    jsp연동

--- src/main/java/com/khj/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/khj/exam/demo/controller/UsrHomeController.java
@@ -1,14 +1,17 @@
 package com.khj.exam.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
 	@RequestMapping("/usr/home/main")
-	@ResponseBody
 	public String showMain() {
-		return "Main입니다.";
+		return "usr/home/main";
+	}
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
 	}
 }
